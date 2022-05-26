72369e27e770651fa8656f790e283eb002236029
unknown <diegotrujillo0210@gmail.com>
2022-05-25 22:23:42 -0500

    empezando Proyecto cvLASS

--- ProyectoFinalIbero/src/proyectofinalibero/Cliente.java
+++ ProyectoFinalIbero/src/proyectofinalibero/Cliente.java
@@ -10,4 +10,121 @@ package proyectofinalibero;
  */
 public class Cliente {
     
+    private String tipoDocumento;
+    private double numeroDocumento;
+    private String nombre;
+    private String apellido;
+    private double telefono;
+    private String direccion;
+    private String email;
+
+    /**
+     * @return the tipoDocumento
+     */
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    /**
+     * @param tipoDocumento the tipoDocumento to set
+     */
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    /**
+     * @return the numeroDocumento
+     */
+    public double getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    /**
+     * @param numeroDocumento the numeroDocumento to set
+     */
+    public void setNumeroDocumento(double numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public double getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(double telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+                /*METODOS  */
+    
+    public int VerificacionUser(){
+    
+        return 0;
+    
+    }
+    
+    
 }
---
+++ ProyectoFinalIbero/src/proyectofinalibero/Reserva.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectofinalibero;
+
+/**
+ *
+ * @author Developer
+ */
+public class Reserva {
+    
+    private String codigo;
+    private String fechaIngreso;
+    private String fechaSalida;
+    private int cantidadPersonas;
+    private String tipoHabitacion;
+
+    /**
+     * @return the codigo
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the fechaIngreso
+     */
+    public String getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    /**
+     * @param fechaIngreso the fechaIngreso to set
+     */
+    public void setFechaIngreso(String fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    /**
+     * @return the fechaSalida
+     */
+    public String getFechaSalida() {
+        return fechaSalida;
+    }
+
+    /**
+     * @param fechaSalida the fechaSalida to set
+     */
+    public void setFechaSalida(String fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    /**
+     * @return the cantidadPersonas
+     */
+    public int getCantidadPersonas() {
+        return cantidadPersonas;
+    }
+
+    /**
+     * @param cantidadPersonas the cantidadPersonas to set
+     */
+    public void setCantidadPersonas(int cantidadPersonas) {
+        this.cantidadPersonas = cantidadPersonas;
+    }
+
+    /**
+     * @return the tipoHabitacion
+     */
+    public String getTipoHabitacion() {
+        return tipoHabitacion;
+    }
+
+    /**
+     * @param tipoHabitacion the tipoHabitacion to set
+     */
+    public void setTipoHabitacion(String tipoHabitacion) {
+        this.tipoHabitacion = tipoHabitacion;
+    }
+    
+}
